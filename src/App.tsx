81fcedb550bb54e675e1093c945e7166246dde63
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-01 21:33:43 +0900

    show suspended

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Suspense } from "react";
 import "./App.css";
 
+function Component1(): JSX.Element {
+  throw new Promise(() => {});
+}
+
 function App() {
-  return <div>app</div>;
+  return (
+    <Suspense fallback={<div>suspended</div>}>
+      <Component1 />
+    </Suspense>
+  );
 }
 
 export default App;
