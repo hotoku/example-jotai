5831c4720d425a3cadb9f56e91b184788223d825
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-02 18:50:24 +0900

    this does not work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import { atom } from "jotai";
-import { useAtomValue } from "jotai";
+import { atom, useAtom } from "jotai";
 import { Suspense } from "react";
+import "./App.css";
 
 function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const listAtom = atom(async () => {
-  await sleep(3000);
-  return ["item a", "item b", "item c"];
-});
+async function download(): Promise<number[]> {
+  await sleep(1500);
+  return [1, 2, 3];
+}
+
+const listAtom = atom([1, 2, 3]);
+const wrappedListAtom = atom(
+  (get) => {
+    return get(listAtom);
+  },
+  async (get, set) => {
+    const x = await download();
+    const v = get(listAtom);
+    const ret: number[] = [];
+    for (let i in x) {
+      ret.push(x[i] + v[i]);
+    }
+    set(listAtom, ret);
+  }
+);
 
 const ListMain = () => {
-  const list = useAtomValue(listAtom);
+  const [list, setList] = useAtom(wrappedListAtom);
   return (
-    <ul>
-      {list.map((item) => (
-        <li>{item}</li>
-      ))}
-    </ul>
+    <div className="app">
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          await setList();
+        }}
+      >
+        update
+      </button>
+    </div>
   );
 };
 
