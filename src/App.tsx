2120e1ab2b6a08f4d25c84df7044ed051be1878c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-02 18:37:24 +0900

    this works

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,31 @@
-import { atom, useAtom } from "jotai";
-import { Suspense, useState } from "react";
-import "./App.css";
+import { atom } from "jotai";
+import { useAtomValue } from "jotai";
+import { Suspense } from "react";
 
-async function download(): Promise<number> {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return 1;
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const dataAtom = atom(1);
-const derivedDataAtom = atom(
-  async (get) => get(dataAtom),
-  async (get, set) => {
-    set(dataAtom, get(dataAtom) + (await download()));
-  }
+export const listAtom = atom(async () => {
+  await sleep(3000);
+  return ["item a", "item b", "item c"];
+});
+
+const ListMain = () => {
+  const list = useAtomValue(listAtom);
+  return (
+    <ul>
+      {list.map((item) => (
+        <li>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => (
+  <Suspense fallback="loading...">
+    <ListMain />
+  </Suspense>
 );
-
-function Component1(): JSX.Element {
-  console.log("render Component1");
-  const [_, setClick] = useState({});
-  const [data, setData] = useAtom(derivedDataAtom);
-  return (
-    <div>
-      <div>data is </div>
-      <div>{data}</div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          console.log("click");
-          setData();
-          setClick({});
-        }}
-      >
-        get data
-      </button>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Suspense fallback={<div>suspended</div>}>
-      <div className="app">
-        <Component1 />
-      </div>
-    </Suspense>
-  );
-}
 
 export default App;
