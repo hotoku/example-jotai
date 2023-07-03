4404f0f435d60638eef22beb84adf00cfce13dff
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-04 00:00:57 +0900

    This is not what I want to do.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import { atom, useAtomValue } from "jotai";
 import { Suspense } from "react";
 import "./App.css";
 
+function sleep(ms: number): Promise<void> {
+  return new Promise((r) => setTimeout(() => r(), ms));
+}
+
 const valAtom = atom<number | undefined>(undefined);
+
+const asyncAtom = atom(async (get) => {
+  await sleep(2000);
+  return 0;
+});
 
 function CompA(): JSX.Element {
   return <button>download</button>;
@@ -16,12 +25,20 @@ function CompB(): JSX.Element {
   return <div>not loaded</div>;
 }
 
+function CompC(): JSX.Element {
+  const val = useAtomValue(asyncAtom);
+  return <div>{val}</div>;
+}
+
 function App(): JSX.Element {
   return (
     <div className="app">
       <CompA />
       <Suspense fallback={<div>loading</div>}>
         <CompB />
+      </Suspense>
+      <Suspense fallback={<div>loading</div>}>
+        <CompC />
       </Suspense>
     </div>
   );
