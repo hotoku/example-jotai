7125b27caf6dcc7d926079cee832d731c525e380
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-03 23:51:25 +0900

    very basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
-import { atom, useAtom } from "jotai";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import "./App.css";
 
-function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function download(): Promise<number> {
-  await sleep(1500);
-  return Math.floor(Math.random() * 100);
-}
-
-const valAtom = atom<number | undefined>(undefined);
-
-function Comp1(): JSX.Element {
-  const [val, setVal] = useAtom(valAtom);
-
-  useEffect(() => {
-    download().then((d) => setVal(d));
-  }, []);
-
-  if (val === undefined) {
-    return <div>loading</div>;
-  }
-  return <div>{val}</div>;
-}
-
-function Comp2(): JSX.Element {
+function CompA(): JSX.Element {
   return <button>download</button>;
+}
+
+function CompB(): JSX.Element {
+  return <div>not loaded</div>;
 }
 
 function App(): JSX.Element {
   return (
     <div className="app">
-      <Suspense>
-        <Comp1 />
-        <Comp2 />
+      <CompA />
+      <Suspense fallback={<div>loading</div>}>
+        <CompB />
       </Suspense>
     </div>
   );
