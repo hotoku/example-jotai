9489b1a74913750ea626dcff8c5095c848cd6d93
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-03 23:53:36 +0900

    initial value

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { atom, useAtomValue } from "jotai";
 import { Suspense } from "react";
 import "./App.css";
+
+const valAtom = atom<number | undefined>(undefined);
 
 function CompA(): JSX.Element {
   return <button>download</button>;
 }
 
 function CompB(): JSX.Element {
+  const val = useAtomValue(valAtom);
+  if (val) {
+    return <div>{val}</div>;
+  }
   return <div>not loaded</div>;
 }
 
